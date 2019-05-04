a6e6dedf6aefe3d3ae6d41cf19b27a29b644cdde
Osman Cakmak <48910999+oxcakmak@users.noreply.github.com>
2019-05-05 01:30:30 +0300

    Create en_EN.php

---
+++ en_EN.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Author: Osman Çakmak
+Skype: oxcakmak
+Email: oxcakmak@hotmail.com
+Website: http://oxcakmak.com/
+Country: Turkey [TR]
+*/
+/*
+Language: English [EN]
+*/
+/*
+[Menu]
+*/
+$lstr['ls_menu_title_home'] = "Home";
+
+/*
+[Label]
+*/
+$lstr['ls_label_title_welcome'] = "Welcome";
+
+/*
+[Button]
+*/
+
+
+/*
+[Table]
+*/
+
+
+/*
+[Message]
+*/
+
+
+/*
+Usage:
+$lstr['ls_menu_title_'] = "";
+$lstr['ls_label_title_'] = "";
+$lstr['ls_button_title_'] = "";
+$lstr['ls_table_title_'] = "";
+$lstr['ls_message_title_'] = "";
+*/
+?>
